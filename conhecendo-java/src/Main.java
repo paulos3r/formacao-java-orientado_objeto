bbc0b449f48d63d7d309e69e92549cc0be262fdd
Paulo Sergio <036paulo@gmail.com>
2024-06-15 17:10:10 -0300

    Projeto inicio imprimir no console o processo de compilacao

--- conhecendo-java/src/Main.java
+++ conhecendo-java/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("Processo de compilacao");
+    System.out.println("");
+    System.out.println("1 - Codigo java");
+    System.out.println("2 - Compilador java");
+    System.out.println("3 - Bytecode / .class");
+    System.out.println("4 - JVM [Java virtual machine] executa o bytecode");
+    System.out.println("5 - Windows / Linux / Mac");
   }
 }
